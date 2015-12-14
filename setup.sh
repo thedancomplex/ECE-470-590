9154378647aa876d44a04f7ce29fa6901dae4335
GMU Student <student@ubuntu.(none)>
2015-12-13 23:43:52 -0500

    added box at x=1.5, y=0, z=1 for arm test

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo cp -n /etc/hubo-ach/sim/openHubo/scenes/floor.env.xml /etc/hubo-ach/sim/openHubo/scenes/floor.env.xml.old
+sudo cp floor.env.xml  /etc/hubo-ach/sim/openHubo/scenes/floor.env.xml
+echo 'done...'
